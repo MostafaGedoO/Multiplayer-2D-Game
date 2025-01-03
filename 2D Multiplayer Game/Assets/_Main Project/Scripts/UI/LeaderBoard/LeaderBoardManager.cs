8e7b70ac184ab579425c979efeaef9c6e710e846
Mostafa Reda <54401606+MostafaGedoO@users.noreply.github.com>
2025-01-03 20:19:50 +0200

    Leaderboard prefab update

--- 2D Multiplayer Game/Assets/_Main Project/Scripts/UI/LeaderBoard/LeaderBoardManager.cs
+++ 2D Multiplayer Game/Assets/_Main Project/Scripts/UI/LeaderBoard/LeaderBoardManager.cs
@@ -1,18 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class LeaderBoardManager : MonoBehaviour
+public class LeaderBoardManager : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [SerializeField] private Transform leaderboardContainer;
+   [SerializeField] private LeaderBoardItem leaderBoardItemPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private NetworkList<LeaderBoardState> leaderBoardStates;
+
+   private void Awake()
+   {
+      leaderBoardStates = new NetworkList<LeaderBoardState>();
+   }
+
+   public override void OnNetworkSpawn()
+   {
+      if (IsClient)
+      {
+         leaderBoardStates.OnListChanged += OnLeaderBoardStateChanged;
+
+         foreach (var _playerState in leaderBoardStates)
+         {
+            OnLeaderBoardStateChanged(new NetworkListEvent<LeaderBoardState>(){Type = NetworkListEvent<LeaderBoardState>.EventType.Add,Value = _playerState });
+         }
+      }
+      
+      if(!IsServer) return;
+      
+      PlayerManager[] _players = FindObjectsByType<PlayerManager>(sortMode: FindObjectsSortMode.None);
+      foreach (PlayerManager _player in _players)
+      {
+         PlayerManager_OnAPlayerSpawn(_player);
+      }
+
+      PlayerManager.OnAPlayerSpawn += PlayerManager_OnAPlayerSpawn;
+      PlayerManager.OnAPlayerDespawn += PlayerManager_OnAPlayerDespawn;
+   }
+
+   public override void OnNetworkDespawn()
+   {
+      if (IsClient)
+      {
+         leaderBoardStates.OnListChanged -= OnLeaderBoardStateChanged;
+      }
+      
+      if(!IsServer) return;
+      
+      PlayerManager.OnAPlayerSpawn -= PlayerManager_OnAPlayerSpawn;
+      PlayerManager.OnAPlayerDespawn -= PlayerManager_OnAPlayerDespawn;
+   }
+   
+   private void OnLeaderBoardStateChanged(NetworkListEvent<LeaderBoardState> _changeevent)
+   {
+      if (_changeevent.Type == NetworkListEvent<LeaderBoardState>.EventType.Add)
+      {
+         Instantiate(leaderBoardItemPrefab, leaderboardContainer);
+      }
+   }
+
+   private void PlayerManager_OnAPlayerSpawn(PlayerManager _player)
+   {
+      leaderBoardStates.Add(new LeaderBoardState(){ClientID = _player.OwnerClientId , PlayerName = _player.playerName.Value , Coins = 0});
+   }
+   
+   private void PlayerManager_OnAPlayerDespawn(PlayerManager _player)
+   {
+      if(leaderBoardStates == null) return;
+      
+      foreach (var playerState in leaderBoardStates)
+      {
+         if(playerState.ClientID != _player.OwnerClientId) continue;
+         leaderBoardStates.Remove(playerState);
+      }
+   }
 }
